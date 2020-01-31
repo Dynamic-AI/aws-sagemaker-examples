30aa8f3e34c5f4c6a32a04109b614896fd1458b2
Ievgen Sliusar <slu@slu.kiev.ua>
2020-01-31 18:42:17 +0200

    dynamic_ai.py: Add get_model_package_arn() helper function

--- src/v1/dynamic_ai.py
+++ src/v1/dynamic_ai.py
@@ -15,6 +15,28 @@ _categories = dict()
 _checkpoint = dict()
 
 
+def get_model_package_arn(current_region=None):
+    if current_region is None:
+        current_region = sagemaker.Session().boto_session.region_name
+
+    mapping = {
+      "ap-northeast-1" : "arn:aws:sagemaker:ap-northeast-1:977537786026:model-package/dynamic-ai-demo-617ce7e562c6bd730e798be57baa4605",
+      "ap-northeast-2" : "arn:aws:sagemaker:ap-northeast-2:745090734665:model-package/dynamic-ai-demo-617ce7e562c6bd730e798be57baa4605",
+      "ap-south-1" :     "arn:aws:sagemaker:ap-south-1:077584701553:model-package/dynamic-ai-demo-617ce7e562c6bd730e798be57baa4605",
+      "ap-southeast-1" : "arn:aws:sagemaker:ap-southeast-1:192199979996:model-package/dynamic-ai-demo-617ce7e562c6bd730e798be57baa4605",
+      "ap-southeast-2" : "arn:aws:sagemaker:ap-southeast-2:666831318237:model-package/dynamic-ai-demo-617ce7e562c6bd730e798be57baa4605",
+      "ca-central-1" :   "arn:aws:sagemaker:ca-central-1:470592106596:model-package/dynamic-ai-demo-617ce7e562c6bd730e798be57baa4605",
+      "eu-central-1" :   "arn:aws:sagemaker:eu-central-1:446921602837:model-package/dynamic-ai-demo-617ce7e562c6bd730e798be57baa4605",
+      "eu-west-1" :      "arn:aws:sagemaker:eu-west-1:985815980388:model-package/dynamic-ai-demo-617ce7e562c6bd730e798be57baa4605",
+      "eu-west-2" :      "arn:aws:sagemaker:eu-west-2:856760150666:model-package/dynamic-ai-demo-617ce7e562c6bd730e798be57baa4605",
+      "us-east-1" :      "arn:aws:sagemaker:us-east-1:865070037744:model-package/dynamic-ai-demo-617ce7e562c6bd730e798be57baa4605",
+      "us-east-2" :      "arn:aws:sagemaker:us-east-2:057799348421:model-package/dynamic-ai-demo-617ce7e562c6bd730e798be57baa4605",
+      "us-west-1" :      "arn:aws:sagemaker:us-west-1:382657785993:model-package/dynamic-ai-demo-617ce7e562c6bd730e798be57baa4605",
+      "us-west-2" :      "arn:aws:sagemaker:us-west-2:594846645681:model-package/dynamic-ai-demo-617ce7e562c6bd730e798be57baa4605"
+    }
+    return mapping[current_region]
+
+
 def deploy_model(model_arn=None, model_name=None, instance_type=None):
     global _model_name, _endpoint_name, _session, _predictor
 
