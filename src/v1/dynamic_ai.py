3916ae22b2fb3d64f92f809089d3660f670dce71
Ievgen Sliusar <slu@slu.kiev.ua>
2020-01-31 18:12:57 +0200

    dynamic_ai.py: Remove extra whitespace

--- src/v1/dynamic_ai.py
+++ src/v1/dynamic_ai.py
@@ -17,19 +17,19 @@ _checkpoint = dict()
 
 def deploy_model(model_arn=None, model_name=None, instance_type=None):
     global _model_name, _endpoint_name, _session, _predictor
-    
+
     if _session is not None:
         print('Previous session is still active, you must shutdown it first. Please use shutdown() function.')
         return
-    
+
     if model_arn is None and model_name is None:
         raise ValueError('Both model_arn adn model_name is None')
-        
+
     # create unique model name and endpoint name
     timestamp = str(int(time.time()))
     _model_name = 'dynamic-ai-model-' + timestamp
     _endpoint_name = 'dynamic-ai-endpoint-' + timestamp
-    
+
     # this line of code requires iam:GetRole permissions
     role = sagemaker.get_execution_role()
 
@@ -38,14 +38,14 @@ def deploy_model(model_arn=None, model_name=None, instance_type=None):
 
     # create a new Model object
     print('Creating model: ' + _model_name)
-    
+
     if model_arn is not None:
         model = sagemaker.model.ModelPackage(sagemaker_session=_session, name=_model_name, model_package_arn=model_arn, role=role)
-    
+
     elif model_name is not None:
         account = _session.boto_session.client('sts').get_caller_identity()['Account']
         region = _session.boto_session.region_name
-        
+
         image = '{}.dkr.ecr.{}.amazonaws.com/{}:latest'.format(account, region, model_name)
         model = sagemaker.model.Model(sagemaker_session=_session, name=_model_name, image=image, model_data=None, role=role)
 
@@ -56,18 +56,18 @@ def deploy_model(model_arn=None, model_name=None, instance_type=None):
     # construct predictor
     _predictor = sagemaker.predictor.RealTimePredictor(endpoint=_endpoint_name, sagemaker_session=_session,
         serializer=sagemaker.predictor.json_serializer, deserializer=sagemaker.predictor.json_deserializer)
-    
+
     # we're done
     print('\nSuccess')
 
-    
+
 def shutdown():
     global _model_name, _endpoint_name, _session, _predictor
-    
+
     if _predictor is not None:
         _predictor.delete_endpoint(delete_endpoint_config=True)
         _predictor.delete_model()
-    
+
     _model_name = None
     _endpoint_name = None
     _session = None
@@ -84,8 +84,8 @@ def get_internal_state():
 def set_internal_state(state):
     global _model_name, _endpoint_name, _session, _predictor, _messages, _categories, _checkpoint
     _model_name, _endpoint_name, _session, _predictor, _messages, _categories, _checkpoint = state
-    
-    
+
+
 def is_ready():
     response = _predictor.predict({"type":"isReady"})
     return bool(response["ready"])
@@ -93,12 +93,12 @@ def is_ready():
 
 def reset():
     _predictor.predict({"type":"reset"})
-    
+
     _messages.clear()
     _categories.clear()
     _checkpoint.clear()
 
-    
+
 def create_checkpoint():
     response = _predictor.predict({"type":"saveCheckpoint"})
     if response['result'] == 'success':
@@ -112,7 +112,7 @@ def create_checkpoint():
 def restore_checkpoint():
     if not _checkpoint:
         raise RuntimeError('To respore checkpoint, you should create it first')
-    
+
     response = _predictor.predict({"type":"restoreCheckpoint"})
     if response['result'] == 'success':
         _messages = dict(_checkpoint['messages'])
@@ -132,13 +132,13 @@ def add_message(message):
                     "message": message,
                     "security_group": "default"
                 })
-            
+
             message_id = response.get('message_id')
             if type(message_id) == str and len(message_id) > 0:
                 _messages[message_id] = message
                 return message_id;
             return None
-        
+
         except:
             attempt += 1
             if attempt == max_attempts:
@@ -161,21 +161,21 @@ def add_feedback(message_id, relations):
     rel = []
     for id, flags in relations:
         rel.append({'id': id, 'flags': flags})
-    
+
     response = _predictor.predict(
         {
             "type": "addFeedback",
             "message_id": message_id,
             "relations": rel
         })
-    
+
     return response.get('result') == 'success'
 
 
 def set_category(message_id, category):
     if message_id not in _messages:
         raise ValueError('Unknown message_id')
-    
+
     relations = list()
     for id, cat in _categories.items():
         if id == message_id:
@@ -187,8 +187,8 @@ def set_category(message_id, category):
 
     _categories[message_id] = category;
     return add_feedback(message_id, relations)
-    
-    
+
+
 def get_similarity(message_id, accuracy_limit=0.6, block_limit=2):
     response = _predictor.predict(
         {
@@ -233,21 +233,21 @@ def get_tech_report(message_id, accuracy_limit=0.6, block_limit=2):
             "block_limit": block_limit
         })
     return response["tech_report"]
-    
+
 
 def predict_category(message_id, accuracy_limit=0.6):
     if message_id not in _messages:
         raise ValueError('Unknown message_id')
-    
+
     if message_id in _categories:
         return {
             'category': _categories.get(message_id),
             'accuracy': 1.0,
             'is_approved': True,
         }
-    
+
     similarity = get_similarity(message_id, accuracy_limit)
-    
+
     for s in similarity:
         id = s.get('message_id');
         if id not in _categories:
@@ -259,7 +259,7 @@ def predict_category(message_id, accuracy_limit=0.6):
             'accuracy': s.get('accuracy'),
             'is_approved': s.get('is_approved'),
         }
-            
+
     return {
         'category': None,
         'accuracy': 0.0,
@@ -272,7 +272,7 @@ def list_messages(category=None):
         return dict(_messages)
     return {id: _messages.get(id) for id, cat in _categories.items() if cat == category}
 
-    
+
 def list_categories():
     return dict(_categories)
 
